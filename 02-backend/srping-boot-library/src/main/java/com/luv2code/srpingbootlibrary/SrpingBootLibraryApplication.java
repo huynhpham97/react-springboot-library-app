3a5de20c49cde24f37ee23d2691bdef7bfa3263c
unknown <huynhphamm1997@gmail.com>
2023-11-15 21:07:05 +0700

    commit folder backend

---
+++ 02-backend/srping-boot-library/src/main/java/com/luv2code/srpingbootlibrary/SrpingBootLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.luv2code.srpingbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingBootLibraryApplication.class, args);
+	}
+}
---
+++ 02-backend/srping-boot-library/src/test/java/com/luv2code/srpingbootlibrary/SrpingBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.srpingbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
